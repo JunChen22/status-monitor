c05344167e2aeeb1278cab9a4c51269de9c86579
Jun Chen <ccjh522@gmail.com>
2024-08-13 15:24:38 -0400

    Add files via upload

---
+++ src/main/java/com/junchen/statusmonitor/StatusmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.junchen.statusmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusmonitorApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/junchen/statusmonitor/service/impl/StatusServiceImpl.java
@@ -0,0 +1,10 @@
+package com.junchen.statusmonitor.service.impl;
+
+import com.junchen.statusmonitor.service.StatusService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StatusServiceImpl implements StatusService {
+
+
+}
